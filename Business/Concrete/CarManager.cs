2ed708086142fb579556250a99f1c28da4232781
Emine <eminetekin97@outlook.com>
2024-01-15 13:21:18 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccesss.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _Icardal;
+
+        public CarManager(ICarDal cardal)
+        {
+            _Icardal = cardal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _Icardal.GetAll();
+        }
+    }
+}
+
---
+++ ConseleUI/Program.cs
@@ -0,0 +1,9 @@
+﻿using Business.Concrete;
+using DataAccesss.Concrete.InMemory;
+
+CarManager carManager = new CarManager(new InMemoryCarDal());
+foreach (var car in carManager.GetAll())
+{
+    string output = $"carId ={car.Id} BrandId={car.BrandId} ColorId={car.ColorId} Description={car.Description} DailyPrice={decimal.ToInt32(car.DailyPrice)}";
+    Console.WriteLine(output);
+}
